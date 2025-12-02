6a19a673e7ff4f9851f3b9808918e1163f5cc721
harishreddygandhudi <harishreddygandhudi@gmail.com>
2025-12-02 11:53:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Add item
+function addToCart(name, price) {
+    let user= localStorage.getItem("loginUser");
+
+//if not logged in-> block add-to-cart
+if(!user){
+    alert("please login first!");
+    window.location.href="Login.html";
+    return;
+}
+
+    cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added!");
+}
+//get the logged in hear
+
+// Show items in cart
+function showCart() {
+    let box = document.getElementById("cart-items");
+    if (!box) return;
+
+    let total = 0;
+    box.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        box.innerHTML += `
+                ${item.name} - $${item.price}
+                <button onclick="removeFromCart(${index})">Remove</button>
+            </p>
+        `;
+        total += item.price;
+    });
+
+    document.getElementById("total").innerText = "Total: $" + total;
+}
+
+// Remove item
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    showCart();
+}
+
+showCart();
+
+
+script.js
